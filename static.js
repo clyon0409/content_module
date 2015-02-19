5cf59d9803a1060bb417aacfa6d23165de5cf374
clyon0409 <cecilia_lyon@sbcglobal.net>
2015-02-19 14:34:00 -0800

    refactored to handle all general url requests

--- static.js
+++ static.js
@@ -1,35 +1,67 @@
 
 //fs module allows us to read and write content for responses!!
 var fs = require('fs');
+var path = require('path');
 
 module.exports = function(request,response){
 	//see what URL the clients are requesting:
 	console.log('hello from my server');
 	console.log('static content got client request URL: ', request.url);
-	//this is how we do routing:
-	if(request.url === '/'){
-		fs.readFile('index.html', 'utf8', function(errors, contents){
-			response.writeHead(200, {'Content-Type': 'text/html'}); //send data about response
-			response.write(contents); // send response body
-			response.end(); //finished!
-		});
+
+
+	var data = path.parse(request.url);
+	var file_path;
+	console.log(data);
+	var content_type;
+	switch(data.ext){
+		case'.jpg':
+		case '.jpeg':
+			content_type = 'image/jpeg';
+			file_path = "."+ data.dir + "/" + data.base;
+			break;
+		case '.png':
+			content_type = 'image/png';
+			file_path = "."+ data.dir + "/" + data.base;
+			break;
+		case '.css':
+			content_type = 'text/css';
+			file_path = "."+ data.dir + "/" + data.base;
+			break;		
+		default:	
+			content_type = 'text/html';
+			if (!data.base)
+			{
+				file_path = "./views/index.html";
+			}
+			else
+			{
+				console.log(data["base"]);
+				file_path = "./views/"+ data["base"] +".html";
+			}
+			break;
 	}
-	else if(request.url === '/ninjas'){
-		fs.readFile('ninjas.html', 'utf8', function(errors, contents){
-			response.writeHead(200, {'Content-Type': 'text/html'}); //send data about response
-			response.write(contents); // send response body
-			response.end(); //finished!
-		});
-	}
-	else if(request.url === '/dojos/new'){
-		fs.readFile('dojos.html', 'utf8', function(errors, contents){
-			response.writeHead(200, {'Content-Type': 'text/html'}); //send data about response
-			response.write(contents); // send response body
-			response.end(); //finished!
-		});
-	}
-	// request didn't match anything:
-	else {
+
+	console.log(file_path);
+
+	if(request.url !== ''){
+		console.log("file: " + file_path + " content_type " + content_type);
+		if (content_type === 'text/html' || content_type === 'text/css' )
+		{
+			fs.readFile(file_path, 'utf8', function(errors, contents){
+				response.writeHead(200, {'Content-Type': content_type}); //send data about response
+				response.write(contents); // send response body
+				response.end(); //finished!
+			});
+		}
+		else
+		{
+			fs.readFile(file_path, function(errors, contents){
+				response.writeHead(200, {'Content-Type': content_type}); //send data about response
+				response.write(contents); // send response body
+				response.end(); //finished!
+			});
+		}
+	}else {
 		response.writeHead(404);
 		response.end('File not found!!!!');
 	}
